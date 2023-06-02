fa1ab28030840c91fc863efb54d3b9188a1d4b2e
danieldiv <das.29@outlook.com>
2023-06-02 15:42:02 -0300

    Update

--- src/init.pl
+++ src/init.pl
@@ -16,6 +16,27 @@ op_principal(autorizacao_guias_3).
 op_principal(manifestacoes_4).
 op_principal(finalizar_6).
 
+op_comercial(portabilidade_1).
+op_comercial(carencia_planos_2).
+op_comercial(inclusao_dependente_3).
+op_comercial(exclusao_dependente_4).
+op_comercial(outros_5).
+
+op_financeiro(negociacao).
+op_financeiro(imposto_renda).
+op_financeiro(segunda_via_boleto).
+op_financeiro(utilizacao_mensalidade).
+op_financeiro(parcelamento).
+
+op_autorizacao_guias(consultar_status_autorizacao).
+op_autorizacao_guias(download_guia_autorizada).
+op_autorizacao_guias(autorizacao_guias).
+op_autorizacao_guias(anexar_documento).
+op_autorizacao_guias(autorizacao_consultas).
+op_autorizacao_guias(revalidacao_guias).
+
+op_manifestacoes(m1).
+
 menu_principal :-
     findall(X, op_principal(X), Lista),
     exibir_lista(Lista).
@@ -56,7 +77,7 @@ cliente :-
                     O = 6 ->
                         finalizar;
                         opcao_invalida).
-                        
+
 check_pessoa :-
     write('Pessoa fisica ou juridica? (f/j): '),
     read(P),
